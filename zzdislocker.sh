3d18ec4358a7678fce5d138e72ad9bc393259294
Zane <info@zane.it>
2018-03-24 15:06:00 +0100

    no message

--- zzdislocker.sh
+++ zzdislocker.sh
@@ -1,4 +1,62 @@
 #!/bin/bash
 clear
 
-echo "Test"
+## Script name
+SCRIPT_NAME=zzdislocker
+
+## Title and graphics
+FRAME="O===========================================================O"
+echo "$FRAME"
+echo "      $SCRIPT_NAME - $(date)"
+echo "$FRAME"
+
+## Enviroment variables
+TIME_START="$(date +%s)"
+DOWEEK="$(date +'%u')"
+HOSTNAME="$(hostname)"
+
+## Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT_FULLPATH=$(readlink -f "$0")
+SCRIPT_HASH=`md5sum ${SCRIPT_FULLPATH} | awk '{ print $1 }'`
+
+## Absolute path this script is in, thus /home/user/bin
+SCRIPT_DIR=$(dirname "$SCRIPT_FULLPATH")/
+
+## Config files
+CONFIGFILE_NAME=$SCRIPT_NAME.conf
+CONFIGFILE_FULLPATH_DEFAULT=${SCRIPT_DIR}$SCRIPT_NAME.default.conf
+CONFIGFILE_FULLPATH_ETC=/etc/turbolab.it/$CONFIGFILE_NAME
+CONFIGFILE_FULLPATH_DIR=${SCRIPT_DIR}$CONFIGFILE_NAME
+
+## Title printing function
+function printTitle
+{
+    echo ""
+    echo "$1"
+    printf '%0.s-' $(seq 1 ${#1})
+    echo ""
+}
+
+## root check
+if ! [ $(id -u) = 0 ]; then
+
+		echo ""
+		echo "vvvvvvvvvvvvvvvvvvvv"
+		echo "Catastrophic error!!"
+		echo "^^^^^^^^^^^^^^^^^^^^"
+		echo "This script must run as root!"
+
+		printTitle "How to fix it?"
+		echo "Execute the script like this:"
+		echo "sudo $SCRIPT_NAME"
+
+		printTitle "The End"
+		echo $(date)
+		echo "$FRAME"
+		exit
+fi
+
+
+printTitle "Detecting Bitlocked disk"
+FDISK="$(fdisk -l | grep 'sd')"
+echo $FDISK
