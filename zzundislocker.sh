b25b95f325c37d17e760bff6abcf0dda6ca96384
Zane <info@zane.it>
2018-03-24 16:43:47 +0100

    zzundislocker

---
+++ zzundislocker.sh
@@ -0,0 +1,77 @@
+#!/bin/bash
+clear
+
+## Script name
+SCRIPT_NAME=zzdislocker
+
+## Title and graphics
+FRAME="O===========================================================O"
+echo "$FRAME"
+echo "      $SCRIPT_NAME - $(date)"
+echo "$FRAME"
+
+## Enviroment variables
+TIME_START="$(date +%s)"
+DOWEEK="$(date +'%u')"
+HOSTNAME="$(hostname)"
+
+## Absolute path to this script, e.g. /home/user/bin/foo.sh
+SCRIPT_FULLPATH=$(readlink -f "$0")
+SCRIPT_HASH=`md5sum ${SCRIPT_FULLPATH} | awk '{ print $1 }'`
+
+## Absolute path this script is in, thus /home/user/bin
+SCRIPT_DIR=$(dirname "$SCRIPT_FULLPATH")/
+
+## Config files
+CONFIGFILE_NAME=$SCRIPT_NAME.conf
+CONFIGFILE_FULLPATH_DEFAULT=${SCRIPT_DIR}$SCRIPT_NAME.default.conf
+CONFIGFILE_FULLPATH_ETC=/etc/turbolab.it/$CONFIGFILE_NAME
+CONFIGFILE_FULLPATH_DIR=${SCRIPT_DIR}$CONFIGFILE_NAME
+
+## Title printing function
+function printTitle
+{
+    echo ""
+    echo "$1"
+    printf '%0.s-' $(seq 1 ${#1})
+    echo ""
+}
+
+## root check
+if ! [ $(id -u) = 0 ]; then
+
+		echo ""
+		echo "vvvvvvvvvvvvvvvvvvvv"
+		echo "Catastrophic error!!"
+		echo "^^^^^^^^^^^^^^^^^^^^"
+		echo "This script must run as root!"
+
+		printTitle "How to fix it?"
+		echo "Execute the script like this:"
+		echo "sudo $SCRIPT_NAME"
+
+		printTitle "The End"
+		echo $(date)
+		echo "$FRAME"
+		exit
+fi
+
+
+printTitle "Read config (if any)"
+for CONFIGFILE_FULLPATH in "$CONFIGFILE_FULLPATH_DEFAULT" "$CONFIGFILE_MYSQL_FULLPATH_ETC" "$CONFIGFILE_FULLPATH_ETC" "$CONFIGFILE_FULLPATH_DIR" "$CONFIGFILE_PROFILE_FULLPATH_ETC" "$CONFIGFILE_PROFILE_FULLPATH_DIR"
+do
+	if [ -f "$CONFIGFILE_FULLPATH" ]; then
+		source "$CONFIGFILE_FULLPATH"
+	fi
+done
+
+printTitle "Unmounting"
+DISLOCKER_MOUNT_FULLPATH=/media/dislocker/dislocked/
+MYDRIVE_MOUNT_FULLPATH=/media/dislocker/MYDRIVE/
+
+umount "$MYDRIVE_MOUNT_FULLPATH"
+umount "$DISLOCKER_MOUNT_FULLPATH"
+
+printTitle "Finish"
+echo "You can now disconnect the drive"
+
