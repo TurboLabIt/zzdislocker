9b25406d5d2983385cb50cf3bc6d4855353ec9b4
Dr. Gianluigi "Zane" Zanettini <gianluigi.zanettini@boraso.com>
2023-04-20 22:34:46 +0200

    revamped setup script

--- setup.sh
+++ setup.sh
@@ -1,74 +1,18 @@
-#!/bin/bash
-clear
-
-## Script name
+#!/usr/bin/env bash
+echo ""
 SCRIPT_NAME=zzdislocker
 
-## Install directory
-WORKING_DIR_ORIGINAL="$(pwd)"
-INSTALL_DIR_PARENT="/usr/local/turbolab.it/"
-INSTALL_DIR=${INSTALL_DIR_PARENT}${SCRIPT_NAME}/
+## bash-fx
+if [ -z $(command -v curl) ]; then sudo apt update && sudo apt install curl -y; fi
+curl -s https://raw.githubusercontent.com/TurboLabIt/bash-fx/master/setup.sh?$(date +%s) | sudo bash
+source /usr/local/turbolab.it/bash-fx/bash-fx.sh
+## bash-fx is ready
 
-## /etc/ config directory
-mkdir -p "/etc/turbolab.it/"
+sudo bash /usr/local/turbolab.it/bash-fx/setup/start.sh ${SCRIPT_NAME}
 
-## Pre-requisites
-apt update
-apt install git gcc cmake make libfuse-dev libmbedtls-dev ruby-dev -y
+fxLinkBin ${INSTALL_DIR}${SCRIPT_NAME}.sh
+fxLinkBin ${INSTALL_DIR}zzundislocker.sh
 
-## Workaround for missing Ruby header
-# https://github.com/Aorimn/dislocker/issues/150#issuecomment-395337603
-cp /usr/include/x86_64-linux-gnu/ruby-2.5.0/ruby/config.h /usr/include/ruby-2.5.0/ruby/
+sudo bash ${INSTALL_DIR}zzdislocker-compile.sh
 
-## Install/update
-echo ""
-if [ ! -d "$INSTALL_DIR" ]; then
-	echo "Installing..."
-	echo "-------------"
-	mkdir -p "$INSTALL_DIR_PARENT"
-	cd "$INSTALL_DIR_PARENT"
-	git clone https://github.com/TurboLabIt/${SCRIPT_NAME}.git
-else
-	echo "Updating..."
-	echo "----------"
-fi
-
-## Fetch & pull new code
-cd "$INSTALL_DIR"
-git pull
-
-## Symlink (globally-available command)
-if [ ! -e "/usr/bin/${SCRIPT_NAME}" ]; then
-	ln -s ${INSTALL_DIR}${SCRIPT_NAME}.sh /usr/bin/${SCRIPT_NAME}
-fi
-
-if [ ! -e "/usr/bin/zzundislocker" ]; then
-        ln -s ${INSTALL_DIR}zzundislocker.sh /usr/bin/zzundislocker
-fi
-
-## Install dislocker
-INSTALL_DIR_PARENT="/opt/"
-INSTALL_DIR=${INSTALL_DIR_PARENT}dislocker/
-
-echo ""
-if [ -d "$INSTALL_DIR" ]; then
-	rm -rf "${INSTALL_DIR}"
-fi
-
-cd "$INSTALL_DIR_PARENT"
-git clone https://github.com/Aorimn/dislocker.git
-cd "$INSTALL_DIR"
-git pull
-cmake .
-make
-make install
-rm -rf "${INSTALL_DIR}"
-
-
-## Restore working directory
-cd $WORKING_DIR_ORIGINAL
-
-echo ""
-echo "Setup completed!"
-echo "----------------"
-echo "See https://github.com/TurboLabIt/${SCRIPT_NAME} for the quickstart guide."
+sudo bash /usr/local/turbolab.it/bash-fx/setup/the-end.sh ${SCRIPT_NAME}
---
+++ zzdislocker-compile.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+source "/usr/local/turbolab.it/bash-fx/bash-fx.sh"
+
+fxHeader "🚀 Compile Dislocker"
+rootCheck
+
+fxEndFooter
